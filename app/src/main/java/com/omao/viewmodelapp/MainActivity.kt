f30b1a56affa4d073120ad09bfd91b9d70c5c40d
omaomach <aaronjoash44@gmail.com>
2023-09-24 22:14:08 +0300

    having challenges with data binding util

--- app/src/main/java/com/omao/viewmodelapp/MainActivity.kt
+++ app/src/main/java/com/omao/viewmodelapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.omao.viewmodelapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 
 class MainActivity : AppCompatActivity() {
+
+    private var counter = 0
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,
+            R.layout.activity_main)
     }
 }
