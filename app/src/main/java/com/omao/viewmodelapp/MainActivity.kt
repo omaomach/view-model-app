a118a6ebe56743ca6dc60c2f2dc6a571d0efbf28
omaomach <aaronjoash44@gmail.com>
2023-10-03 10:48:32 +0300

    implemented ViewModel through MainActivityViewModel

--- app/src/main/java/com/omao/viewmodelapp/MainActivity.kt
+++ app/src/main/java/com/omao/viewmodelapp/MainActivity.kt
@@ -3,22 +3,25 @@ package com.omao.viewmodelapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import com.omao.viewmodelapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private var counter = 0
     private lateinit var binding: ActivityMainBinding
+    lateinit var viewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = DataBindingUtil.setContentView(this,
             R.layout.activity_main)
 
-        binding.textView1.text = counter.toString()
+        viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
+
+        binding.textView1.text = viewModel.getCurrentCount().toString()
         binding.btn1.setOnClickListener() {
-            counter++
-            binding.textView1.text = counter.toString()
+            binding.textView1.text = viewModel.getUpdatedCount().toString()
         }
 
     }
---
+++ app/src/main/java/com/omao/viewmodelapp/MainActivityViewModel.kt
@@ -0,0 +1,17 @@
+package com.omao.viewmodelapp
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel: ViewModel() {
+
+    private var counter = 0
+
+    fun getCurrentCount(): Int {
+        return  counter
+    }
+
+    fun getUpdatedCount(): Int {
+        return ++counter
+    }
+
+}
